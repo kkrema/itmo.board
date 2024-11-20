252f6dd067a471f3cc5fc2de72e754a3283cfd9b
Nikita <fk724s@mail.ru>
2024-11-20 20:56:54 +0300

    prettier code style

--- frontend/sentry.server.config.ts
+++ frontend/sentry.server.config.ts
@@ -2,10 +2,10 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  dsn: process.env.SENTRY_SERVER_DSN,
-  tracesSampleRate: 1,
-  debug: false,
+    dsn: process.env.SENTRY_SERVER_DSN,
+    tracesSampleRate: 1,
+    debug: false,
 });
