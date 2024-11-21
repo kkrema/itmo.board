bd506d709971bbea53cbf01cb7a671df3cf6d69f
Vera Kasianenko <112972833+CandyGoose@users.noreply.github.com>
2024-11-21 12:32:35 +0300

    return svg

--- frontend/app/[locale]/(dashboard)/boards/[boardId]/_components/Rectangle.tsx
+++ frontend/app/[locale]/(dashboard)/boards/[boardId]/_components/Rectangle.tsx
@@ -18,19 +18,21 @@ export const Rectangle = ({
     const {x, y, width, height, fill} = layer;
 
     return (
-        <rect
-            className="drop-shadow-md"
-            onPointerDown={(e) => onPointerDown(e, id)}
-            style={{
-                transform: `translate(${x}px, ${y}px) `,
-            }}
-            x={0}
-            y={0}
-            width={width}
-            height={height}
-            strokeWidth={1}
-            fill={fill ? colorToCss(fill) : "#000"}
-            stroke={selectionColor || "transparent"}
-        />
+        <svg>
+            <rect
+                className="drop-shadow-md"
+                onPointerDown={(e) => onPointerDown(e, id)}
+                style={{
+                    transform: `translate(${x}px, ${y}px) `,
+                }}
+                x={0}
+                y={0}
+                width={width}
+                height={height}
+                strokeWidth={1}
+                fill={fill ? colorToCss(fill) : "#000"}
+                stroke={selectionColor || "transparent"}
+            />
+        </svg>
     );
 };
