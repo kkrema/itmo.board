a0d5c4091c836db79a61eecdc843693655fafe60
Nikita <fk724s@mail.ru>
2024-11-24 14:48:05 +0300

    add intl to next config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,12 @@
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin();
+
 const nextConfig = {
     reactStrictMode: true,
     eslint: {
         ignoreDuringBuilds: true,
-    },
+    }
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
