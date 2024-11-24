5088a269ac4b928546b90310d8ab8b7e1129be37
Nikita <fk724s@mail.ru>
2024-11-24 14:48:59 +0300

    prettier

--- frontend/i18n/routing.ts
+++ frontend/i18n/routing.ts
@@ -6,4 +6,5 @@ export const routing = defineRouting({
     defaultLocale: 'en',
 });
 
-export const { Link, redirect, usePathname, useRouter } = createNavigation(routing);
+export const { Link, redirect, usePathname, useRouter } =
+    createNavigation(routing);
